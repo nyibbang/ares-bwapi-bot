1789c2ea6cef83d703844ed432d1971f9a742c9a
Vincent PALANCHER <vincent.palancher@gmail.com>
2015-02-02 21:29:59 +0100

    Adding build system (CMake) base files, and a few dummy source files to test it

---
+++ Core.cpp
@@ -0,0 +1,8 @@
+#include "Core.h"
+
+/* Empty file just to test the build system */
+int foo()
+{
+    return 42;
+}
+
---
+++ test/TestCore.cpp
@@ -0,0 +1,8 @@
+#include "Core.h"
+#include <gtest/gtest.h>
+
+TEST(FooTest, FooReturns42)
+{
+  EXPECT_EQ(foo(), 42);
+}
+
