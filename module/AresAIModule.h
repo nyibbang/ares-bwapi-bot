fbac961c718f5b53fae58e8fbaee0800b7db588b
Vincent PALANCHER <vincent.palancher@gmail.com>
2015-02-16 00:48:14 +0100

    Creating module subdirectory with BWAPI::AIModule example source code

---
+++ module/AresAIModule.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <BWAPI.h>
+
+class AresAIModule : public BWAPI::AIModule
+{
+    public:
+        virtual void onStart();
+        virtual void onEnd(bool isWinner);
+        virtual void onSaveGame(std::string gameName);
+        virtual void onFrame();
+        virtual void onSendText(std::string text);
+        virtual void onReceiveText(BWAPI::Player player, std::string text);
+        virtual void onPlayerLeft(BWAPI::Player player);
+        virtual void onNukeDetect(BWAPI::Position target);
+        virtual void onUnitDiscover(BWAPI::Unit unit);
+        virtual void onUnitEvade(BWAPI::Unit unit);
+        virtual void onUnitShow(BWAPI::Unit unit);
+        virtual void onUnitHide(BWAPI::Unit unit);
+        virtual void onUnitCreate(BWAPI::Unit unit);
+        virtual void onUnitDestroy(BWAPI::Unit unit);
+        virtual void onUnitMorph(BWAPI::Unit unit);
+        virtual void onUnitRenegade(BWAPI::Unit unit);
+        virtual void onUnitComplete(BWAPI::Unit unit);
+};
+
