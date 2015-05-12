0fcd55eb39591ed1f45303ebe1f9a76a54a03d78
Vincent PALANCHER <vincent.palancher@gmail.com>
2015-05-12 23:50:04 +0200

    Renamed corelib into core, moved traceslib to core and renamed it trace, made trace build under linux

--- core/trace/Trace.cpp
+++ core/trace/Trace.cpp
@@ -1,10 +1,28 @@
+/*
+ * AresBWAPIBot : A bot for SC:BW using BWAPI library;
+ * Copyright (C) 2015 Vincent PALANCHER; Florian LEMEASLE
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 
-#include "traces.h"
+#include "Trace.h"
 #ifdef ON_WINDOWS
 #include <windows.h>
 #include <shlobj.h>
 #include <ctime>
-
 
 namespace
 {
@@ -148,5 +166,5 @@ traces::Facade& traces::Facade::instance()
     return instance;
 }
 
-
 #endif
+
--- core/trace/Trace.h
+++ core/trace/Trace.h
@@ -1,6 +1,26 @@
+/*
+ * AresBWAPIBot : A bot for SC:BW using BWAPI library;
+ * Copyright (C) 2015 Vincent PALANCHER; Florian LEMEASLE
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
 #pragma once
 
-#if defined(_WIN32) || defined(_WIN64) || defined(__GNUC__) || defined(__MINGW32__)
+#if defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__)
 #   define ON_WINDOWS
 #endif
 
@@ -104,7 +124,6 @@ class Facade final
 
 }
 
-
 template<class T> traces::Stream& traces::Stream::operator<<(T&& t)
 {
     m_buffer << std::forward<T>(t);
@@ -132,3 +151,4 @@ void traces::Facade::initializeAuxiliaryLogger(TArgs&&... args)
     #define ARES_WARNING ARES_ERROR
     #define ARES_ERROR() if(false) std::cout
 #endif
+
